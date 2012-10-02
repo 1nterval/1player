a73050e745786dcb2ce5b9a00fb3a9cd7f20a034
Fabien Quatravaux <fabien.quatravaux@1nterval.com>
2012-10-02 11:23:21 +0200

    [fix] Refaire marcher mediaelementjs

--- players/mediaelementjs/mediaelementjs.php
+++ players/mediaelementjs/mediaelementjs.php
@@ -2,27 +2,29 @@
 
 add_action('wp_print_scripts', 'mediaelementjs_add_scripts');
 function mediaelementjs_add_scripts(){
-    $options = get_option('player');
-    wp_enqueue_script("mediaelementjs",  plugins_url('mediaelement-and-player.min.js', __FILE__ ), array(), "2.9.1", false);
+    $options = get_option('player_video');
+    wp_enqueue_script("mediaelementjs",  plugins_url('mediaelement-and-player.min.js', __FILE__ ), array('jquery'), "2.9.1", false);
     wp_enqueue_style("mediaelementjs",  plugins_url('mediaelementplayer.css', __FILE__ ), array(), "2.9.1");
     if($options['skin'] != 'none') 
         wp_enqueue_style($options['skin'], plugins_url('skins/'.$options['skin'].'/mejs-skin.css', __FILE__ ), array("mediaelementjs"));
 }
 
-add_action('player_render', 'mediaelementjs_render');
+add_action('mediaelementjs_video_render', 'mediaelementjs_render');
 function mediaelementjs_render($args){
-    $options = get_option('player');
+    global $mime_types;
+    $options = get_option('player_video');
     
     $attributes = "";
     if($args['controls'] != 'none') $attributes .= " controls";
     if($args['loop']) $attributes .= " loop";
     if($args['autoplay']) $attributes .= " autoplay";
 
-	?><video <?php echo $attributes ?> <?php if($options['skin'] != "none") echo 'class="'.$options['skin'].'"' ?> id="player<?php echo $args['instance'] ?>" poster="<?php echo $args['videos'][0]['poster'] ?>" width="<?php echo $args['width'] ?>" height="<?php echo $args['height'] ?>">
-	    <?php foreach(array('flash', 'html5') as $mode): ?>
-	        <source src="<?php echo $args['videos'][0][$mode]['src'] ?>" type="video/<?php echo array_pop(explode('.', $args['videos'][0][$mode]['src'])) ?>">
-	    <?php endforeach; ?>
-	</video>
+    ?><video <?php echo $attributes ?> <?php if($options['skin'] != "none") echo 'class="'.$options['skin'].'"' ?> id="player<?php echo $args['instance'] ?>" poster="<?php echo $args['videos'][0]['poster'] ?>" width="<?php echo $args['width'] ?>" height="<?php echo $args['height'] ?>">
+        <?php foreach($args['videos'][0]['html5']['src'] as $video): ?>
+            <source src="<?php echo $video['src'] ?>" <?php if($video['compat'] != 'none') echo 'type="'.$mime_types[$video['compat']].'"' ?>>
+        <?php endforeach; ?>
+    </video>
+	
 	<script>
 	    var args = {};
 	    
@@ -37,10 +39,11 @@ function mediaelementjs_render($args){
 	        case "html5flash": $mode = "auto"; break;
 	    } ?>
 	    
-	    <?php if($options['mode'] == "flashhtml5") : /* forcer le mode flash seulement si flash est installé */ ?>
-	        if(navigator.mimeTypes == undefined || navigator.mimeTypes["application/x-shockwave-flash"] != undefined)
-	    <?php endif; ?>
-	    args.mode = "<?php echo $mode ?>";
+        <?php if($options['mode'] == "flashhtml5") : /* forcer le mode flash seulement si flash est installé */ ?>
+            if(navigator.mimeTypes == undefined || navigator.mimeTypes["application/x-shockwave-flash"] != undefined)
+        <?php endif; ?>
+        args.mode = "<?php echo $mode ?>";
+        args.pluginPath = "<?php echo plugins_url('/', __FILE__ ) ?>";
 	    
         var mejs_player = new MediaElementPlayer("#player<?php echo $args['instance'] ?>", args);
         
@@ -70,6 +73,13 @@ function mediaelementjs_skins_description($desc){
 add_filter('1player_controls_positions_list', 'mediaelementjs_controls_positions_list');
 function mediaelementjs_controls_positions_list($list){
     $list["fixed"] = __("Fixed", "1player");
+    return $list;
+}
+
+add_filter('1player_modes_list', 'mediaelementjs_modes_list');
+function mediaelementjs_modes_list($list){
+    $list["html5flash"] = __('HTML5 if possible, else Flash','1player');
+    $list["flashhtml5"] = __('Flash if possible, else HTML5','1player');
     return $list;
 }
 
